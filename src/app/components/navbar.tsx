b443fc1d01b17adacd19946d0ea4b5bac3880dab
Max <72516602+maxas10@users.noreply.github.com>
2025-07-03 01:00:21 -0700

    mobile support 2

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,6 @@
 
 "use client";
-import { motion } from "motion/react"
 import Link from "next/link"
-import { useState } from "react"
 export default function Navbar() {
     return (
         <div className="flex flex-col w-72 justify-center items-center gap-5 h-screen fixed bg-black" id="navbar">
