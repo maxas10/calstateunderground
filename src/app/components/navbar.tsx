03e91f29dfa39e28970ca65ad9e0ef6d11f1ee2c
Max <72516602+maxas10@users.noreply.github.com>
2025-07-03 02:00:18 -0700

    added poppins as font file

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="flex flex-col w-72 justify-center items-center gap-5 h-screen fixed bg-black" id="navbar">
-            <Link href="/"><img src="logo.jpg" alt="" className="rounded-full mb-5 logo-image" /></Link>
+            <Link href="/"><img src="logo.jpg" alt="" className="rounded-full mb-5" /></Link>
             <button><Link href="/about">about</Link></button>
             <button><Link href="/covers">covers</Link></button>
             <button><Link href="/songs">songs</Link></button>
